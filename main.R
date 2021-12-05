8b0eb62ec1c345339fc954f9ac3fbc805c52f7b2
jamesmikely <jamesmichaely@gmail.com>
2021-12-05 22:15:38 +0300

    Initial

---
+++ main.R
@@ -0,0 +1,48 @@
+#
+# This is a Plumber API. You can run the API by clicking
+# the 'Run API' button above.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+library(openair)
+
+#* @apiTitle Plumber Example API
+#* @apiDescription Plumber example description.
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg = "") {
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Plot a histogram
+#* @serializer pdf
+#* @get /plot
+function() {
+  # load example data from package
+  data(mydata)
+
+  # basic plot
+  calendarPlot(mydata, pollutant = "o3", year = 2003)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+  as.numeric(a) + as.numeric(b)
+}
+
+# Programmatically alter your API
+#* @plumber
+function(pr) {
+  pr %>%
+    # Overwrite the default serializer to return unboxed JSON
+    pr_set_serializer(serializer_unboxed_json())
+}
